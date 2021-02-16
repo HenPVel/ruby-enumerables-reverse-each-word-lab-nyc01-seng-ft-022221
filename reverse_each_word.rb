ba387fff4982321fd0858be7622c33d601175ad2
Henry Velasquez <hvelas93@gmail.com>
2021-02-16 17:09:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,6 +12,6 @@
 
 def reverse_each_word(string_argument)
 
-  string_argument.collect{ |element| element.reverse}
-  new_string.join(" ")
+  [string_argument.collect{ |element| element.reverse}].join(" ")
+  
   end
