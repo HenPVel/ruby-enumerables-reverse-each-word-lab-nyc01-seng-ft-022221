cfaf43069aa6aa13bc9e92d9e5a906ba2f7ee0b5
Henry Velasquez <hvelas93@gmail.com>
2021-02-16 17:08:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,17 @@
 
 #string_argument="Hello there, and how are you?"
 
-def reverse_each_word(string_argument)
-  new_string=[]
-  x=string_argument.split
-  x.each{ |element| new_string<<element.reverse}
-  new_string.join(" ")
-  end
+# def reverse_each_word(string_argument)
+#   new_string=[]
+#   x=string_argument.split
+#   x.each{ |element| new_string<<element.reverse}
+#   new_string.join(" ")
+#   end
 
 #p reverse_each_word(string_argument)
+
+def reverse_each_word(string_argument)
+
+  x.collect{ |element| element.reverse}
+  new_string.join(" ")
+  end
