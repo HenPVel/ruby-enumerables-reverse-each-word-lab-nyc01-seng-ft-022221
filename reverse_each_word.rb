79e80cb433b551f350624766ddae1b6b892791df
Henry Velasquez <hvelas93@gmail.com>
2021-02-16 16:59:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ string_argument="Hello there, and how are you?"
 def reverse_each_word(string_argument)
   new_string=[]
   x=string_argument.split
-  x.each{ |element| element.reverse<<new_string}
+  x.each{ |element| new_string<<element.reverse}
   end
 
 p reverse_each_word(string_argument)
